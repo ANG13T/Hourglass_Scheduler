480dcd2a13a04dd08b1acc18038b313fe8990337
angelina-tsuboi <angelina.t1832@gmail.com>
2021-05-15 13:52:55 -0700

    feat: added html template

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const path = require("path");
 const router = express.Router();
 
 const app = express();
+app.use(express.static('app_files/assets'))
 const port = process.env.PORT || "8000";
 
 router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/app_files//index.html'));
+    res.sendFile(path.join(__dirname+'/app_files/index.html'));
     //__dirname : It will resolve to your project folder.
   });
   
