2d1b67b0c03f22ad2c3840e8f13f0bc908911ca7
angelina-tsuboi <angelina.t1832@gmail.com>
2021-05-15 13:46:00 -0700

    feat: connected templates to server

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const path = require("path");
+const router = express.Router();
 
 const app = express();
 const port = process.env.PORT || "8000";
 
-
-app.get("/", (req, res) => {
-    res.status(200).send("Hello World");
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/app_files//index.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+  
+router.get('/about',function(req,res){
+res.sendFile(path.join(__dirname+'/app_files/about.html'));
 });
+  
+//add the router
+app.use('/', router);
+
 
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
