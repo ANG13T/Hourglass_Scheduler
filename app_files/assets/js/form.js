7dbfad9e5b1bb351a7b6aeb9ee9769157684366a
angelina-tsuboi <angelina.t1832@gmail.com>
2021-05-16 06:03:07 -0700

    feat: saved form data into local storage

--- app_files/assets/js/form.js
+++ app_files/assets/js/form.js
@@ -2,10 +2,17 @@ $("#name-invalid-feedback").css("display", "none");
 var formData = {
     name: "",
     email: "",
-    
+
 }
 
 
 function holdFormData(){
+    formData.name = $("#name").val();
+    formData.email = $("#email").val();
+}
 
+function postFormData(){
+    alert("posting");
+    holdFormData();
+    localStorage.setItem("formData", JSON.stringify(formData));
 }
