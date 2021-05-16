15d4c1b22b4a288c2223e75243c010c3aceb9ad9
angelina-tsuboi <angelina.t1832@gmail.com>
2021-05-16 07:28:10 -0700

    update schedule

--- app_files/assets/js/schedule.js
+++ app_files/assets/js/schedule.js
@@ -1,4 +1,4 @@
-let userData;
+var userData;
 
 function getData(){
   let data = localStorage.getItem("formData");
@@ -7,6 +7,36 @@ function getData(){
   console.log(userData, typeof userData);
 }
 
-window.onload = function(e){ 
-  getData();
+
+function constructEventData(){
+  if(!userData){
+    console.log("undefined");
+    return [];
+  }
+
+  let events = [];
+  events.push({
+    title: "Sleep", 
+    start: moment(userData["startSleep"]),
+    end: moment(addHours(userData["startSleep"], parseInt(userData["amountSleep"]))),
+    dow: [ 1, 4 ]
+  })
+  console.log("events", events);
+  return events;
 }
+
+const pad = n => {
+  const s = String(n);
+  return s.length > 1 ? s : '0' + s;
+};
+
+const addHours = (timeVal, numHours) => {
+  const [hr, min, sec] = timeVal.split(':').map(Number);
+  const [,lefty, righty] = String(numHours).match(/(\d+)(?:(\.\d+))?/).map(Number);
+  const hours = (hr + lefty) % 24;
+  const minutes = righty === undefined ? 
+    min : 
+    ((righty * 60 | 0) + min) % 60;
+
+  return [hours, minutes, sec].map(pad).join(':');  
+};
